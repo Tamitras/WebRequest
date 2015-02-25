e645ee38352cab5063fa39a26f607a9eda18f311
Tamitras <erik.kaufmann@t-online.de>
2015-02-25 17:53:04 +0100

    1. Stand --> Laden von SpielerNamen für die Datenbank

---
+++ WebRequest/WebRequest/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WebRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxPlayers = new System.Windows.Forms.ListBox();
+            this.BtnPlayerToDB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(529, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(279, 83);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Suchen";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(30, 485);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(98, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(529, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ziel Link:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Suchen nach:";
+            // 
+            // listBoxPlayers
+            // 
+            this.listBoxPlayers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxPlayers.FormattingEnabled = true;
+            this.listBoxPlayers.Location = new System.Drawing.Point(30, 126);
+            this.listBoxPlayers.Name = "listBoxPlayers";
+            this.listBoxPlayers.Size = new System.Drawing.Size(597, 355);
+            this.listBoxPlayers.TabIndex = 6;
+            // 
+            // BtnPlayerToDB
+            // 
+            this.BtnPlayerToDB.Location = new System.Drawing.Point(483, 487);
+            this.BtnPlayerToDB.Name = "BtnPlayerToDB";
+            this.BtnPlayerToDB.Size = new System.Drawing.Size(144, 23);
+            this.BtnPlayerToDB.TabIndex = 7;
+            this.BtnPlayerToDB.Text = "Spieler übernehmen";
+            this.BtnPlayerToDB.UseVisualStyleBackColor = true;
+            this.BtnPlayerToDB.Click += new System.EventHandler(this.BtnPlayerToDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 520);
+            this.Controls.Add(this.BtnPlayerToDB);
+            this.Controls.Add(this.listBoxPlayers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxPlayers;
+        private System.Windows.Forms.Button BtnPlayerToDB;
+    }
+}
+
---
+++ WebRequest/WebRequest/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebRequest
+{
+    public partial class Form1 : Form
+    {
+        String SeitenQuellCode { get; set; }
+        String Zeichenkette { get; set; }
+        Int32 Seite { get; set; }
+        Boolean SeitenAnzalende { get; set; }
+
+        List<String> ListOfPlayers { get; set; }
+        List<String> ListOfCurrentPlayerInDataBase { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            Seite = 1;
+            textBox1.Text = "http://www.futhead.com/15/players/?page=" + Seite + "&sort_direction=desc";
+            Zeichenkette = "<a href=\"/15/players/";
+            SeitenAnzalende = false;
+            ListOfPlayers = new List<string>();
+            ListOfCurrentPlayerInDataBase = new List<string>();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<String> list = new List<string>();
+            while (!SeitenAnzalende)
+            {
+                SeitenQuellCode = getHTML(textBox1.Text);
+                if (null == SeitenQuellCode)
+                {
+                    SeitenAnzalende = true;
+                    break;
+                }
+                textBox2.Text = SeitenQuellCode;
+
+                list.AddRange(SearchForString());
+                if (list.Count > 0)
+                {
+                    textBox2.Text = String.Empty;
+                    textBox2.Text += String.Format("{0} Spieler gefunden" + Environment.NewLine, list.Count);
+                    foreach (String item in list)
+                    {
+                        // - wird gegen ein " " getauscht 
+                        String temp = item.Replace("-", " ");
+
+                        // Anfangsbuchstabe von Vorname und Nachname werden groß gemacht
+                        //listBoxPlayers.Items.Add(temp);
+
+                        ListOfPlayers.Add(temp);
+
+                        //Debugging
+                        //if (ListOfPlayers.Count == 50)
+                        //{
+                        //    SeitenAnzalende = true;
+                        //}
+                    }
+                    list.Clear();
+
+                    Seite++;
+                    Int32 percent = 100 * Seite / 246;
+                    this.Text = Seite.ToString() + " " + percent + "%";
+
+                    textBox1.Text = "http://www.futhead.com/15/players/?page=" + Seite + "&sort_direction=desc";
+                }
+                else
+                {
+                    SeitenAnzalende = true;
+                }
+            }
+        }
+
+
+        public string getHTML(string url)
+        {
+            //Anfrage an die Übergebene URL starten
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            String html = "";
+            try
+            {
+                //Antwort-Objekt erstellen
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                //Antwort Stream an Streamreader übergeben
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+
+                //Antwort (HTML Code) auslesen
+                html = sr.ReadToEnd();
+
+                //Streamreader und Webanfrage schließen
+                sr.Close();
+                response.Close();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+
+            //Quellcode zurückgeben
+            return html;
+        }
+
+        private List<String> SearchForString()
+        {
+            List<String> list = new List<String>();
+            int findname_number = 0;
+            while ((findname_number = SeitenQuellCode.IndexOf(Zeichenkette)) != 0)
+            {
+
+                if (findname_number < 0)
+                {
+                    return list;
+                }
+                else
+                {
+                    string substring = SeitenQuellCode.Substring(findname_number, 21); // Zeichenkette wird gespeichert
+                    SeitenQuellCode = SeitenQuellCode.Remove(0, findname_number + 21); // Alles, vor und einschließlich der Zeichenkette wird gelöscht
+                    //list.Add(substring);
+
+                    Int32 count = 0;
+                    String tempString = String.Empty;
+
+                    foreach (Char item in SeitenQuellCode)
+                    {
+                        if (char.IsNumber(item))
+                        {
+                            count++;
+
+                        }
+                        else
+                        {
+                            if (count > 0)
+                            {
+                                String name = "";
+                                foreach (Char element in SeitenQuellCode)
+                                {
+                                    name += element;
+                                    if (name.Contains("/"))
+                                    {
+                                        SeitenQuellCode = SeitenQuellCode.Remove(0, name.Length);
+                                        if (!char.IsNumber(name[0]))
+                                        {
+
+                                            String temp = name.Remove(name.Length - 1);
+                                            temp = temp.Replace("-", " ");
+
+                                            if (null == ListOfCurrentPlayerInDataBase.SingleOrDefault(c=> c == temp))
+                                            {
+                                                //if (ValidatePlayer(temp))
+                                                //{
+                                                //    ;
+                                                //}
+                                                //else // Wurde "false" zurückgegeben, so gibt es noch keinen Eintrag mit diesem Namen in der DB --> Füge der Liste hinzu
+                                                //{
+                                                //    list.Add(temp);
+                                                //    ListOfCurrentPlayerInDataBase.Add(temp);
+                                                //}
+
+                                                list.Add(temp);
+                                                ListOfCurrentPlayerInDataBase.Add(temp);
+                                            }
+
+                                            name = "";
+                                            break;
+                                        }
+                                        name = "";
+                                        continue;
+                                    }
+                                }
+                                //SeitenQuellCode = SeitenQuellCode.Remove(0, findname_number +21);
+
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+        private Boolean ValidatePlayer(String playerName)
+        {
+            WebRequest.Linq.PlayerDataContext context = new Linq.PlayerDataContext(WebRequest.Properties.Settings.Default.FutheadConnectionString3);
+            var temp = context.Players.SingleOrDefault(c => c.Name == playerName);
+
+            // Wenn ein Spieler mit dem Namen in der DB gefunden wurde - gib False zurück
+            if (temp == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void BtnPlayerToDB_Click(object sender, EventArgs e)
+        {
+            WebRequest.Linq.PlayerDataContext context = new Linq.PlayerDataContext(WebRequest.Properties.Settings.Default.FutheadConnectionString3);
+
+            List<WebRequest.Linq.Player> linqPlayers = new List<Linq.Player>();
+
+            try
+            {
+                foreach (String item in ListOfPlayers)
+                {
+                    WebRequest.Linq.Player player = new Linq.Player();
+                    player.Name = item;
+                    linqPlayers.Add(player);
+                    if (linqPlayers.Count > 100)
+                    {
+                        context.Players.InsertAllOnSubmit(linqPlayers);
+                        context.SubmitChanges();
+                        linqPlayers.Clear();
+                    }
+                }
+
+                context.Players.InsertAllOnSubmit(linqPlayers);
+                context.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler: " + ex.Message);
+            }
+        }
+    }
+}
